fd0b6ebd7bf940b11d6dd1b6711619bf69724ca7
Wesley Osborne <wao218@lehigh.edu>
2018-03-30 10:00:40 -0400

    Section 3: Hello React, Conditional Rendering in JSX

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ console.log('App.js is running!');
 
 var app = {
     title: 'Indecison App',
-    subtitle: 'Put your life in the hands of a computer'
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['one', 'two']
 };
 
 // JSX - JavaScript XML
 var template = ( 
     <div>
         <h1>{app.title}</h1> 
-        <p>{app.subtitle}</p>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options' : 'No options' }</p>
         <ol>
             <li>Item one</li>
             <li>Item two</li>
@@ -22,11 +24,16 @@ var user = {
     age: 22,
     location: 'Boston'
 };
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    } 
+}
 var templateTwo = (
     <div>
-        <h1>{user.name}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location}</p>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 );
 
