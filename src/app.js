180ddae4bc8b00302793234213b84c76cdf483de
Wesley Osborne <wao218@lehigh.edu>
2018-03-30 09:32:41 -0400

    Section 3: Hello React, Lectures 1 - 5

---
+++ src/app.js
@@ -0,0 +1,36 @@
+console.log('App.js is running!');
+
+var app = {
+    title: 'Indecison App',
+    subtitle: 'Put your life in the hands of a computer'
+};
+
+// JSX - JavaScript XML
+var template = ( 
+    <div>
+        <h1>{app.title}</h1> 
+        <p>{app.subtitle}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var user = {
+    name: 'Wesley',
+    age: 22,
+    location: 'Boston'
+};
+var templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
+
