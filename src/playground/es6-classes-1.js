8911671eebf1d23b0c917cee90e8674937e0755a
Wesley Osborne <wao218@lehigh.edu>
2018-04-01 15:54:07 -0400

    Section 4: React Components, ES6 Classes: Part II

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -12,10 +12,43 @@ class Person {
   }
 }
 
-const me = new Person('Andrew Mead', 26);
-console.log(me.getGreeting());
-console.log(me.getDiscription());
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age); // calls parent class constructor
+    this.major = major;
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  getDiscription() {
+    let description = super.getDiscription();
+    
+    if (this.hasMajor()) {
+      description += ` Their major is ${this.major}.`;
+    }
 
-const other = new Person();
+    return description;
+  }
+}
+
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+  getGreeting() {
+    let greeting = super.getGreeting();
+
+    if (this.homeLocation) {
+      greeting += ` I'm visiting from ${this.homeLocation}.`;
+    }
+    
+    return greeting;
+  }
+}
+
+const me = new Traveler('Andrew Mead', 26, 'Philadelphia');
+console.log(me.getGreeting());
+
+const other = new Traveler(undefined, undefined, 'Nowhere');
 console.log(other.getGreeting());
-console.log(other.getDiscription());
