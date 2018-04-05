697ba56fcd8f479fe73861e7bb60966033a7337c
Wesley Osborne <wao218@lehigh.edu>
2018-04-05 17:20:59 -0400

    Section 6: Webpack, Importing NPM Modules

--- src/app.js
+++ src/app.js
@@ -1,12 +1,6 @@
-// import './utils.js';
-// import subtract, { square, add } from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 81));
+const template = React.createElement('p', {}, 'testing 123');
 
-import isSenior, { isAdult, canDrink } from './person';
-console.log(isAdult(18));
-console.log(canDrink(18));
-console.log(isSenior(65));
+ReactDOM.render(template, document.getElementById('app'));
