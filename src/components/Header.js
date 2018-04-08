3fa4e5197b91c7086b49c2e9112b59501931897e
Wesley Osborne <wao218@lehigh.edu>
2018-04-07 20:47:33 -0400

    Section 9: React-Router, Query Strings and URL Parameters

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,6 @@ const Header = () => (
     <br/>
     <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
     <br/>
-    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-    <br/>
     <NavLink to="/help" activeClassName="is-active">Help</NavLink>
   </header>
 );
