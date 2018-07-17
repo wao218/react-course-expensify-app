0e1d444ea46e2c0dbe3fed193e86b2ac54a80bb8
Wesley Osborne <wao218@lehigh.edu>
2018-07-17 17:03:27 -0400

    Section 12: Testing Your Application, Mocking Libraries with Jest

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import 'react-dates/lib/css/_datepicker.css';
+import 'normalize.css/normalize.css';
 import AppRouter from './router/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
-import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
 
 const store = configureStore();
 
