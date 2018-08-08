6fdc1172b4dd9753dbbd2cdc6bed24636526c988
Wesley Osborne <wao218@lehigh.edu>
2018-08-07 22:18:03 -0400

    Section 15: Firebase with Redux, Creating a Separate Test Database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyC_xs98EBtNguUsfiuIxjVZuvyk7Z-ToSw',
-  authDomain: 'expensify-f58e6.firebaseapp.com',
-  databaseURL: 'https://expensify-f58e6.firebaseio.com',
-  projectId: 'expensify-f58e6',
-  storageBucket: 'expensify-f58e6.appspot.com',
-  messagingSenderId: '702487841918'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
