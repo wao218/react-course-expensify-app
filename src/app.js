324ebc08d606edf4ba0ac115de8432adac5b41ef
Wesley Osborne <wao218@lehigh.edu>
2018-08-17 07:15:56 -0400

    Section 16: Firebase Authentication, Login Page and Google Authentication

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import './styles/styles.scss';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore();
 
@@ -23,4 +23,12 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 store.dispatch(startSetExpenses()).then(() => {
   ReactDOM.render(jsx, document.getElementById('app'));
+});
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log('log in');
+  } else {
+    console.log('log out');
+  }
 });
